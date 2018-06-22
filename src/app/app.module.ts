8b6908721c3690bcbe58137d1ba4b116b7c1f2f7
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2018-06-22 15:42:33 -0400

    Basic functionality of the todo list with a mock backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,31 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckDouble } from '@fortawesome/free-solid-svg-icons';
+import { faCheckDouble, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import {FormsModule, NgModel} from "@angular/forms";
 
 //Setup the font-awesome icons we are going to use
 library.add(faCheckDouble);
+library.add(faCheckCircle);
+library.add(faCircle);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckboxComponent,
+    TodoItemComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule.forRoot(),
     FontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Todo} from "./model/Todo";
+import {Observable} from "rxjs/internal/Observable";
+import {of} from "rxjs/internal/observable/of";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todos: Todo[] = [
+    new Todo(1, "Learn Angular", true),
+    new Todo(1, "Profit??", false)
+  ];
 
-  constructor() { }
+  constructor() {
+  }
+
+  getTodoList(): Observable<Todo[]> {
+    return of(this.todos);
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
 }
