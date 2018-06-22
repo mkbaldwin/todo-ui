a22dcd67c8a3afe97e0cde458bf2eca3762877c8
Michael Baldwin <mkbaldwin@users.noreply.github.com>
2018-06-22 13:42:17 -0400

    Some initial config adding font-awesome and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckDouble } from '@fortawesome/free-solid-svg-icons';
+
+//Setup the font-awesome icons we are going to use
+library.add(faCheckDouble);
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
